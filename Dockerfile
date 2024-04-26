6adad9feee87966ecb183cbf9810a07b3e48fd79
digitalscream <lee@digitalscream.org.uk>
2024-04-26 21:05:24 +0000

    Ollama experimental build, tested badly with multiple GPUs

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,27 @@
-FROM intelanalytics/ipex-llm-xpu:2.1.0-SNAPSHOT
+FROM intelanalytics/ipex-llm-xpu:latest
 
 # Disable pip's cache behavior
 ARG PIP_NO_CACHE_DIR=false
 
-# Install Serving Dependencies
-RUN cd /llm && \
-    pip install --pre --upgrade ipex-llm[xpu,serving] && \
-    pip install transformers==4.36.2 gradio==4.19.2
-    
-
-RUN apt-get update -y && apt-get install -y git-lfs
-RUN pip3 install fschat
-RUN pip3 install fschat[model_worker,webui] pydantic==1.10.15
-RUN pip3 install autoawq==0.1.8 accelerate==0.25.0 einops
 
 VOLUME [ "/llm", "/root/.cache/huggingface" ]
 
-# This COPY is only necessary until IPEX 2.5.0 is released - it fixes a bug loading Mistral models
-COPY config.py /usr/local/lib/python3.9/dist-packages/ipex_llm/vllm/
-COPY startup.sh /bin/start_fastchat.sh
-RUN chmod 755 /bin/start_fastchat.sh
-
 WORKDIR /llm/
-EXPOSE 7860 8000
 
-ENV FS_ENABLE_WEB=true FS_ENABLE_OPENAI_API=true LOGDIR=/logs SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=1 USE_XETLA=OFF
+RUN apt update && apt dist-upgrade -y && apt install -y clinfo
 
-ENTRYPOINT [ "/bin/bash", "/bin/start_fastchat.sh" ]
+# Install dependencies
+RUN cd /llm && \
+    pip install --pre --upgrade ipex-llm[cpp]
+    
+    
+#RUN curl -fsSL https://ollama.com/install.sh | sh
+
+EXPOSE 11423 11423
+
+COPY llama3.model /llm/bin/
+COPY startup.sh /bin/start_ollama.sh
+RUN chmod 755 /bin/start_ollama.sh
+
+ENTRYPOINT [ "/bin/bash", "/bin/start_ollama.sh" ]
+# docker run -it --name "lj_ollama" --device /dev/dri -p 11434:11434 -v /main_data/docker/llm_models/:/llm/ -v /main_data/docker/ollama_models/:/root/.ollama/ lj_ol:latest --model-path mistral
--- startup.sh
+++ startup.sh
@@ -3,56 +3,26 @@
 
 source /opt/intel/oneapi/setvars.sh
 
-# Find the value for --model-path using sed
-model_path=$(echo "$@" | awk -F'--model-path ' '{print $2}' | awk '{print $1}')
-model_revision=$(echo "$@" | awk -F'--model-revision ' '{print $2}' | awk '{print $1}')
-
-# Extract the model name after the "/" character
-short_model_name=${model_path#*/}
-
-# Print args
-echo "Model path: $model_path"
-echo "Model name: $short_model_name"
-echo "Worker args: $@"
-echo "Enable web: $FS_ENABLE_WEB"
-echo "Enable OpenAI API: $FS_ENABLE_OPENAI_API"
-
-# Start the controller
-python3 -m fastchat.serve.controller --host 0.0.0.0 &
-
-source ipex-llm-init -j -g
+source ipex-llm-init -c -g
 
 export USE_XETLA=OFF
-export SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=1
+export SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=2
+export ZES_ENABLE_SYSMAN=1
 
-# Start the model worker - take your pick, but vLLM is by far the fastest of these options
-# python3 -m fastchat.serve.model_worker --device xpu --trust-remote-code --host 0.0.0.0 $@ &
-python3 -m ipex_llm.serving.fastchat.vllm_worker --device xpu --trust-remote-code --host 0.0.0.0  $@ &
-# python3 -m ipex_llm.serving.fastchat.ipex_llm_worker --trust-remote-code --device "xpu" $@ --host 0.0.0.0 &
-# python3 -m ipex_llm.serving.fastchat.bigdl_worker --model-path $model_path --trust-remote-code --device xpu --host 0.0.0.0 $@ &
+# Use this if you have multiple Intel GPUs and don't want to use them all - 
+# card selector can be an integer, or comma-separated integers
+# export ONEAPI_DEVICE_SELECTOR=level_zero:0,1
 
-# Health check for controller using a test message
-while true; do
-  response=$(python3 -m fastchat.serve.test_message --model-name $short_model_name)
-  if echo "$response" | grep -q "worker_addr: http://localhost:21002"; then
-    echo "Model registered spinning up services..."
-    break
-  else
-    echo "Waiting for model..."
-  fi
-  sleep 3  # wait before the next attempt\
-done
+cd /llm/bin
+
+init-llama-cpp
+init-ollama
+
+OLLAMA_KEEP_ALIVE="-1" OLLAMA_HOST="0.0.0.0:11434" ./ollama serve &
+
+sleep 2
+
+./ollama create llama_local -f llama3.model
 
 
-# Check to see if the web server should be enabled
-if [[ "${FS_ENABLE_WEB}" == "true" ]]; then
-  # Start the web server
-  echo "Enabling web server..."
-  python3 -m fastchat.serve.gradio_web_server --host 0.0.0.0 --model-list-mode 'reload' &
-fi
-
-if [[ "${FS_ENABLE_OPENAI_API}" == "true" ]]; then
-    # Start the OpenAI API
-    echo "Enabling OpenAI API server..."
-    python3 -m fastchat.serve.openai_api_server --host 0.0.0.0 --port 8000
-fi
+/bin/bash
